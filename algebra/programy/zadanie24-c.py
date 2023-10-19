93fd7272fb11728ea15943f0ec774864c92ee59d
Rafał <rafal.piotr.wlodarczyk@gmail.com>
2023-10-19 13:49:52 +0200

    algebra  cw 24

---
+++ algebra/programy/zadanie24-c.py
@@ -0,0 +1,18 @@
+import sys
+if len(sys.argv) != 2:
+    print(f"Usage {sys.argv[0]} [n] - where n is a number in Cn")
+    sys.exit(1)
+
+n = int(sys.argv[1])
+
+if n <= 2:
+    print("n must be greater than 2")
+    sys.exit(1)
+
+print(f"Addition table of C{n}, a + b (mod {n})")
+print('+  ' + ' '.join([str(i) for i in range(n)]))
+for i in range(n):
+    print(f"{i}:", end=" ")
+    for j in range(n):
+        print((i+j) % n, end=" ")
+    print('')
